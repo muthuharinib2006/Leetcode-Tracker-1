545835cfd797d597ac3cb322db3782b3eed20266
muthuharinib2006 <muthuharinib2000@gmail.com>
2026-08-15 20:30:27 +0530

    Create

---
+++ problem-1786806026146/version/java/problem-1786806026146_20260815_203026.java
@@ -0,0 +1,12 @@
+// Last updated: 8/15/2026, 8:30:26 PM
+1class Solution {
+2    public int elevatorRequests(int n, int[] requests) {
+3        int cF=0;
+4        int tT=0;
+5        for(int floor:requests){
+6            tT+=Math.abs(floor-cF);
+7            cF=floor;
+8        }
+9        return tT;
+10    }
+11}
